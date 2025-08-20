4c63db8adf2234401dcf147b7e0647b2bab818c0
Tango <maxim.ant07@gmail.com>
2025-08-20 19:45:54 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,20 @@
 
 int main()
 {
-	std::string resourcePath = RESOURCES_PATH;
-	tng::JSONParser parser;
-	parser.parseToJSON_object(resourcePath + "smthCool.txt");
-}
+	std::string tmpMessage = "{\"escapes\": \"Line\nTab\tQuote\"}";
+	tng::JSONLexer lexer; 
+	try
+	{
+		lexer.tokenize(tmpMessage);
+	}
+	catch (const tng::JSONException& excp)
+	{
+		std::cout << std::format("The reason of exception: {}\n", excp.what());
+	}
+	
+	std::cout << std::format("Token: {} Type: {}\n", lexer.currentToken().mDefinition, lexer.getTokenType());
+	for (size_t i = 0; i < lexer.getNumberTokens()-1; ++i)
+	{
+		std::cout << std::format("Type: {} Token: {}\n", lexer.getTokenType(), lexer.nextToken().mDefinition);
+	}
+} 
